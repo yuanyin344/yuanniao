a2625aa3903ebc673db773165acdee70472ff381
yuanyin344 <136953936+yuanyin344@users.noreply.github.com>
2023-12-18 16:46:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 def freet(name):
  return "Hello " + name + "!"
-  app=gr.Interface(fn=freet,inputs="text",outputs="text")
-  if _name_ == " _main_":
-    app.launch(inbrowser=True)
+ app=gr.Interface(fn=freet,inputs="text",outputs="text")
+ if _name_ == " _main_":
+   app.launch(inbrowser=True)
