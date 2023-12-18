1cc7ac51cdbce41dcee1873e3291999daf8730db
yuanyin344 <136953936+yuanyin344@users.noreply.github.com>
2023-12-18 17:28:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ def freet(name):
  return "Hello " + name + "!"
 app=gr.Interface(fn=freet,inputs="text",outputs="text")
 if __name__ == "__main__":
-   app.launch(inbrowser=True)
+   app.launch(inbrowser=True,share=True)
